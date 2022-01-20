83df41f4cac8925102af26ca37912ef670eb51b0
Maximiliano <maximiliano_diaz@gmail.com>
2022-01-20 20:10:15 -0300

    Add test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,12 +1,19 @@
 require_relative '../src/solver'
 
 describe Solver do
+  solver = Solver.new
   context 'When testing the factorial method' do
     it 'should return the factorial of n' do
       n = 5
-      solver = Solver.new
       expect(solver.factorial(0)).to eq 1
       expect(solver.factorial(n)).to eq 120
+    end
+  end
+
+  context 'When testing the reverse method' do
+    it 'should return the the string `word` in reverse order' do
+      word = 'hello'
+      expect(solver.reverse(word)).to eq 'olleh'
     end
   end
 end
